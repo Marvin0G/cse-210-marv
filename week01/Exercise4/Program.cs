60a3fd826de358e9b72c113f9355120d1e1ea94a
Marvin0G <titaniumxxxoriginal@gmail.com>
2025-09-15 16:05:26 +0000

    average

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,17 +1,20 @@
-using SCollctions.Generic;
+using System.Collections.Generic;
 using System.Reflection.Metadata;
 using System.Linq;
+List<int> numberlist;
+numberlist = new List<int>();
 Console.WriteLine("welcome to my numbers program ");
 
 Console.WriteLine("Enter a list of numbers, type 0 when finished. ");
 
 string numbers = Console.ReadLine();
+        
 
 int number = int.Parse(numbers);
 
+numberlist.Add(number);
 
-List<int> numberlist;
-numberlist = new List<int>();
+int numbercount = 1;
 
 
 
@@ -22,9 +25,14 @@ do
         Console.WriteLine("please input your number");
         number = int.Parse(Console.ReadLine());
         numberlist.Add(number);
+        
     }
-    
+    numbercount = numbercount+1;
 } while (number != 0);
-
+    numbercount = numbercount-1;
     int total = numberlist.Sum();
+    int average = total / numbercount;
+    Console.WriteLine(numbercount);
+    Console.WriteLine($"your average is {average}");
+
     Console.WriteLine($"the sum of all numbers is {total}");
