ddbac253fc7cb6de0f18306a15b5f0703a25bbfb
Marvin0G <TitaniumxXxoriginal@gmail.com>
2025-09-22 11:37:18 +0100

    week 2 finish today

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,44 +1,56 @@
 using System;
+using System.Runtime.CompilerServices;
 
-class Program
-{
-    static void Main(string[] args)
-    {
+
         Console.WriteLine("Hello World! This is the Exercise5 Project.");
 
-        static string displaywelcome()
+        static void Displaywelcome()
         {
-            Console.WriteLine("welcome to my program");
-
+           Console.WriteLine("welcome to my program");
+            
         }
 
-        static string promptusername()
+        static string Promptusername()
         {
-            Console.Write("please input your name");
+            Console.Write("please input your name: ");
             string name = Console.ReadLine();
             // int favnumber = int.Parse(fnumber);
             return name;
 
         }
 
-        static int promptusernumber()
+        static int Promptusernumber()
         {
-            Console.Write("please input your favourite number");
+            Console.Write("please input your favourite number: ");
             string fnumber = Console.ReadLine();
             int favnumber = int.Parse(fnumber);
             return favnumber;
 
         }
 
-        static int squarenumber(int numb)
+        static int Squarenumber(int numb)
         {
-            int squareonumber = numb ^ 6;
+            int squareonumber = numb * numb ;
             return squareonumber;
         }
-        static void displayresult(string name, int sqnumb)
+        static void Displayresult(string name, int sqnumb)
         {
-            Console.WriteLine($"your name is {name} and the square of your favorite number is  {sqnumb}");
+            Console.WriteLine($"your name is {name} and the square of your favorite number is {sqnumb}");
         }
 
-    }
+        // static void Main()
+        // {
+
+            
+        // }
+
+    
+    static void Main()
+{
+    Displaywelcome();
+    string tname = Promptusername();
+    int number = Promptusernumber();
+    int bsqnumb = Squarenumber(number);
+    Displayresult(tname, bsqnumb);
 }
+Main();
--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,9 +1,92 @@
 using System;
 
-class Program
+static void Main(string[] args)
 {
-    static void Main(string[] args)
+    Console.WriteLine("Hello World! This \n is the journal Project.");
+    Console.WriteLine("what would you like to do");
+    Console.WriteLine("1. write");
+    Console.WriteLine("1. Display");
+    Console.WriteLine("1. Load");
+    Console.WriteLine("1. Save");
+    Console.WriteLine("1. Quit");
+    string selectedoption = Console.ReadLine();
+    int selectednumber = int.Parse(selectedoption);
+    string placeholdertext = "";
+    if (selectednumber == 1)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        journal.addentry(placeholdertext);
+    }
+    //     if else (selectednumber == 1)
+    // {
+    //     journal.addentry(placeholdertext);
+    // }
+
+
+
+
+}
+    
+
+    static void hello()
+{
+    Console.WriteLine("Hello World! This is the done Project.");
+}
+
+
+hello();
+
+
+
+
+public class Entry
+{
+    public int date;
+    public string prompt;
+    public string response;
+
+    public static void display()
+    {
+
     }
 }
+
+
+
+
+
+public class journal
+{
+    public static void addentry(string entry)
+    {
+
+    }
+
+    public static void displayall()
+    {
+
+    }
+
+    public static void savetofile(string filename)
+    {
+
+    }
+
+
+    public static void loadfromfile(string filename)
+    {
+
+    }
+}
+
+
+public class promptgenerator
+{
+    public static void getrandomprompt()
+    {
+
+    }
+}
+
+
+
+
