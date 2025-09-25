73cf936846833c2c1cdd1f7c4deafa69945444ce
Marvin0G <TitaniumxXxoriginal@gmail.com>
2025-09-25 15:50:04 +0100

    ugfihdhi

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,25 +1,59 @@
 using System;
-using System.Formats.Tar;
-using System.Collections.Generic; 
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.CompilerServices;
 
 public class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This \n is the journal Project.");
-        Console.WriteLine("what would you like to do");
-        Console.WriteLine("1. write");
-        Console.WriteLine("2. Display");
-        Console.WriteLine("3. Load");
-        Console.WriteLine("4. Save");
-        Console.WriteLine("5. Quit");
-        string selectedoption = Console.ReadLine();
-        int selectednumber = int.Parse(selectedoption);
-        if (selectednumber == 1)
-        {
-            Console.WriteLine("You chose to write an entry!");
+    {   Journal myjournal = new Journal();
+        Promptgenerator generator = new Promptgenerator();
 
+        while (true)
+        {
+            Console.WriteLine("Hello World! This \n is the journal Project.");
+            Console.WriteLine("what would you like to do");
+            Console.WriteLine("1. write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+            string selectedoption = Console.ReadLine();
+            int selectednumber = int.Parse(selectedoption);
+
+
+            if (selectednumber == 1)
+            {
+                string prompt1243 = generator.getrandomprompt();
+                Console.WriteLine(prompt1243);
+                string response = Console.ReadLine();
+
+                Entry newEntry = new Entry(prompt1243, response);
+                myjournal.Addentry(newEntry);
+            }
+
+            else if (selectednumber == 2)
+            {
+                myjournal.displayall();
+            }
+            else if (selectednumber == 3)
+            {
+                Console.Write("enter name of file to be loaded");
+                string filename = Console.ReadLine();
+                myjournal.loadfromfile(filename);
+            }
+            else if (selectednumber == 4)
+            {
+                Console.Write("enter file name to be saved ");
+                string filename2 = Console.ReadLine();
+                myjournal.savetofile(filename2);
+            }
+            else if (selectednumber == 5)
+            {
+                Console.WriteLine("Goodbye");
+            }
         }
+
 
 
 }
@@ -34,13 +68,20 @@ public class Program
 
 public class Entry
 {
-    public string date;
-    public string prompt;
-    public string response;
+    public string date { get; set; }
+    public string prompt { get; set; }
+    public string response { get; set; }
+
+    public Entry(string prompt, string response)
+    {
+    this.date = DateTime.Now.ToString("yyyy-MM-dd");
+    this.prompt = prompt;
+    this.response = response; 
+    }
 
     public static void display()
     {
-
+        Console.WriteLine("");
     }
 }
 
@@ -51,7 +92,7 @@ public class Entry
 public class Journal
 {
     private List<Entry> entries = new List<Entry>();
-    public void addentry(Entry entry)
+    public void Addentry(Entry entry)
     {
         entries.Add(entry);
     }
@@ -64,15 +105,47 @@ public class Journal
         }
     }
 
-    public static void savetofile(string filename)
+    public void savetofile(string filename)
     {
-
+        using (var writer = new StreamWriter(filename))
+        {
+            foreach (Entry e in entries)
+            {
+                writer.WriteLine($" {e.date} | {e.prompt}| {e.response}| ");
+            }
+        }
+        Console.WriteLine($" saved {entries.Count} to {filename}");
     }
 
 
-    public static void loadfromfile(string filename)
+    public void loadfromfile(string filename)
     {
+        if (!File.Exists(filename))
+        {
+            Console.WriteLine("file was not found");
+            return;
+        }
+        entries.Clear();
+        string[] lines = File.ReadAllLines(filename);
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split("|");
+            if (parts.Length >= 3)
+            {
+                string date = parts[0];
+                string prompt = parts[1];
+                string response = string.Join("|", parts, 2, parts.Length - 2);
 
+                Entry e = new Entry(prompt, response);
+                e.date = date;
+                entries.Add(e);
+
+
+
+            }
+        }
+        Console.WriteLine($"loaded {entries.Count} entries from {filename}");
+        
     }
 }
 
